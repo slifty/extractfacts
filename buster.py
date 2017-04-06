aba6c4ee78ee08fda19872b4795b90b36f75a017
Asa Royal <oneroyalace@gmail.com>
2017-04-06 12:54:11 -0500

    add method to visit each transcript

--- buster.py
+++ buster.py
@@ -5,6 +5,9 @@ import psycopg2
 import re
 import requests
 
+transcript_ids = []
+#connect sql 
+"""
 try:
 	try:
 		conn = psycopg2.connect("dbname = 'claims' user = 'postgres' host = 'localhost' password = '...'")
@@ -15,7 +18,7 @@ cur = conn.cursor()
 
 sqlClaims = '''INSERT INTO claims(speaker, claim, transcript_id) VALUES (%(speaker)s, %(claim)s, %(transcript_id)s'''
 sql
-
+"""
 
 #get date in CNN URL format
 def getFormattedDate():
@@ -28,34 +31,53 @@ def cnnLink():
 	return 'http://transcripts.cnn.com/TRANSCRIPTS/'+getFormattedDate()+'.html'
 
 def findNewTranscripts(mainPageLink):
-	return 'http://transcripts.cnn.com/TRANSCRIPTS/......'
+	#transcript_ids = []
+	baseLink = 'http://transcripts.cnn.com'
+	page = requests.get(mainPageLink).text
+	soup = BeautifulSoup(page, 'html.parser')
+	soup.prettify()
+	for piece in soup.find_all('div', {'class': 'cnnSectBulletItems'}):
+		for anchor in piece.find_all('a', href = True):
+			print(baseLink+anchor['href'])
+			#transcript_ids.append(anchor['href'])
+			print()
 
+	return transcript_ids
+
+
+
+
+findNewTranscripts(cnnLink())
 
 #scrape a transcript
+"""
 def scrapeFeed():
-	#curPage = requests.get(cnnLink())
-	personStatements = {}
-	speakerIndeces = [] 
-	indeces = [] #indeces of beginning of new speaker statements
-	speakerChunks = []
+	for transcript in transcript_ids:
 
-	#future: curPage = requests.get(findNewTranscripts(cnnLink()))
-	curPage = requests.get('http://transcripts.cnn.com/TRANSCRIPTS/1703/31/sn.01.html').text 
-	soup = BeautifulSoup(str(curPage), 'html.parser')
-	soup.prettify()
-	transcript = soup.find_all('p',{'class': 'cnnBodyText'})[-1]
-	stringscript = re.sub('\(.*[A-Z].*[A-Z].*\)', '', str(transcript).replace('<br>','\n')) #remove html tags and transition statements
+		personStatements = {}
+		speakerIndeces = [] 
+		indeces = [] #indeces of beginning of new speaker statements
+		speakerChunks = []
+
+		#future: curPage = requests.get(findNewTranscripts(cnnLink()))
+		curPage = requests.get('http://transcripts.cnn.com/TRANSCRIPTS/1703/31/sn.01.html').text 
+		soup = BeautifulSoup(str(curPage), 'html.parser')
+		soup.prettify()
+		transcript = soup.find_all('p',{'class': 'cnnBodyText'})[-1]
+		stringscript = re.sub('\(.*[A-Z].*[A-Z].*\)', '', str(transcript).replace('<br>','\n')) #remove html tags and transition statements
 
 
-	prevStart = 0
-	for m in re.finditer('.*[A-Z].*[A-Z].*?:', stringscript):
-		speakerIndeces.append((m.start(), m.end()))
-		indeces.append(m.end())
-		if prevStart: speakerChunks.append(stringscript[prevStart:m.start()])
-		prevStart = m.start()
-		
+		prevStart = 0
+		for m in re.finditer('.*[A-Z].*[A-Z].*?:', stringscript):
+			speakerIndeces.append((m.start(), m.end()))
+			indeces.append(m.end())
+			if prevStart: speakerChunks.append(stringscript[prevStart:m.start()])
+			prevStart = m.start()
+			
 
-	for indices, chunk in zip(speakerIndeces, speakerChunks):
+		for indices, chunk in zip(speakerIndeces, speakerChunks):
+
+"""
 
 	#for index in range(len(indices)):
 		#speakerChunks.add(stringscript)	
@@ -66,9 +88,7 @@ def scrapeFeed():
 	
 	
 
-#cnnLink()
-scrapeFeed()
+
+#scrapeFeed()
 #while(1):
 #	doInserts()
-
-
