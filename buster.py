e3ea3d0b1d2d1bd06f6d2d7ae8c50f2c3894bba8
Asa Royal <oneroyalace@gmail.com>
2017-04-06 09:22:36 -0400

    separated methods

--- buster.py
+++ buster.py
@@ -1,33 +1,66 @@
 from bs4 import BeautifulSoup
 from datetime import datetime
 from pytz import timezone
+import psycopg2
 import re
 import requests
-"""
-1. Constantly scrape cnn page looking for new transcripts
-2. 
-"""
-def  timeNow():
+
+try:
+	try:
+		conn = psycopg2.connect("dbname = 'claims' user = 'postgres' host = 'localhost' password = '...'")
+	except:
+		print("Can't connect to the database")
+
+cur = conn.cursor()
+
+sqlClaims = '''INSERT INTO claims(speaker, claim, transcript_id) VALUES (%(speaker)s, %(claim)s, %(transcript_id)s'''
+sql
+
+
+#get date in CNN URL format
+def getFormattedDate():
 	zone =  timezone('EST')
 	dateOnly = datetime.now(zone).strftime('%Y.%m.%d')
 	return dateOnly
 
+#return today's CNN transcript page URL
 def cnnLink():
-	return 'http://transcripts.cnn.com/TRANSCRIPTS/'+timeNow()+'.html'
+	return 'http://transcripts.cnn.com/TRANSCRIPTS/'+getFormattedDate()+'.html'
 
+def findNewTranscripts(mainPageLink):
+	return 'http://transcripts.cnn.com/TRANSCRIPTS/......'
+
+
+#scrape a transcript
 def scrapeFeed():
 	#curPage = requests.get(cnnLink())
 	personStatements = {}
+	speakerIndeces = [] 
+	indeces = [] #indeces of beginning of new speaker statements
+	speakerChunks = []
 
-	curPage = requests.get('http://transcripts.cnn.com/TRANSCRIPTS/1703/31/sn.01.html').text
+	#future: curPage = requests.get(findNewTranscripts(cnnLink()))
+	curPage = requests.get('http://transcripts.cnn.com/TRANSCRIPTS/1703/31/sn.01.html').text 
 	soup = BeautifulSoup(str(curPage), 'html.parser')
 	soup.prettify()
-
-
 	transcript = soup.find_all('p',{'class': 'cnnBodyText'})[-1]
 	stringscript = re.sub('\(.*[A-Z].*[A-Z].*\)', '', str(transcript).replace('<br>','\n')) #remove html tags and transition statements
 
-	print(stringscript)
+
+	prevStart = 0
+	for m in re.finditer('.*[A-Z].*[A-Z].*?:', stringscript):
+		speakerIndeces.append((m.start(), m.end()))
+		indeces.append(m.end())
+		if prevStart: speakerChunks.append(stringscript[prevStart:m.start()])
+		prevStart = m.start()
+		
+
+	for indices, chunk in zip(speakerIndeces, speakerChunks):
+
+	#for index in range(len(indices)):
+		#speakerChunks.add(stringscript)	
+
+	#print(stringscript)
 
 
 	
