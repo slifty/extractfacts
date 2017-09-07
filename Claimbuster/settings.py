99335ee1c148ba798ed6bf471fe2a79a9e3b455c
Asa Royal <oneroyalace@gmail.com>
2017-09-06 22:51:19 -0400

    updated templates

--- Claimbuster/settings.py
+++ Claimbuster/settings.py
@@ -100,8 +100,12 @@ if 'RDS_DB_NAME' in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'practice',
+            'USER': 'postgres',
+            'PASSWORD': 'butt',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
